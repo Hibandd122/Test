209b5f260c80266fb68d39d67861dc88b0bb2330
Hibandd122 <130156106+Hibandd122@users.noreply.github.com>
2025-07-02 20:53:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import base64
 import string
 from PIL import Image
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # ✅ Thêm dòng này
 
 import torch
 import torch.nn as nn
@@ -49,6 +50,7 @@ transform = transforms.Compose([
 
 # ==== Flask App ====
 app = Flask(__name__)
+CORS(app)  # ✅ Kích hoạt CORS
 
 @app.route("/", methods=["POST"])
 def predict_base64():
